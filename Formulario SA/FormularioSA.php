2fb058f0603451f91a3fedbf7ad0d7c0011befad
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-07-17 20:34:34 -0300

    update

--- Formulario SA/FormularioSA.php
+++ Formulario SA/FormularioSA.php
@@ -6,17 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-if (isset($_GET['tipo']) && isset($_GET['voltagem']) && isset($_GET['marca']) && isset($_GET['qtda']) && isset($_GET['preco']) && isset($_GET['data']))
-{
-    echo "Recebido o Nome : ".$_GET['tipo'];
-    echo "Recebido a Voltagem : ".$_GET['voltagem']."w";
-    echo "Recebido a Marca : ".$_GET['marca'];
-    echo "Recebido a Quantidade : ".$_GET['qtda'];
-    echo "Recebido o Preço : ".$_GET['preco'];
-    echo "Recebido a Data de validade : ".$_GET['data'];
-}
-?>
+    <?php
+    if (isset($_GET['tipo1']) && isset($_GET['voltagem1']) && isset($_GET['marca1']) && isset($_GET['qtda1']) && isset($_GET['preco1']) && isset($_GET['data1']))
+    {
+        echo "<b>Recebido o Nome :</b> ".$_GET['tipo1'];
+        echo "<br><br>";
+        echo ":<b>Recebido a Voltagem : :</b>".$_GET['voltagem1']."w";
+        echo "<br><br>";
+        echo ":<b>Recebido a Marca : :</b>".$_GET['marca1'];
+        echo "<br><br>";
+        echo ":<b>Recebido a Quantidade : :</b>".$_GET['qtda1'];
+        echo "<br><br>";
+        echo ":<b>Recebido o Preço : :</b>".$_GET['preco1'];
+        echo "<br><br>";
+        echo ":<b>Recebido a Data de validade : :</b>".$_GET['data1'];
+    } else{
+        echo ":<b>ERRO NO CADASTRO:</b>";
+    }
+    ?>
     
 </body>
 </html>
