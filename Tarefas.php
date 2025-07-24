75d8d656346cddb385a60a45d1a7f966da9b8ec0
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-07-23 22:14:42 -0300

    update

--- Tarefas.php
+++ Tarefas.php
@@ -1,27 +1,11 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciiador de tarefas</title>
-
-</head>
-<body>
-    <h1>Gerenciador de Tarefas</h1>
-    <!--Aqui irá o restante do codigo!-->
-    <form>
-        <fieldset>
-            <legend>Nova Tarefa</legend>
-            <label>
-                Tarefa:
-                <input type="text" name="nome" />
-</label>
-<input type="submit" value="Cadastrar" />
-</fieldset>
-</form>
 <?php
-$lista_tarefas = array();
+session_start();
+    if (isset($_GET['nome']) && $_GET['nome'] != ''){
+        $tarefa = array();
+
+        $tarefa['nome'] = $_GET['nome'];
+    }
+
     if (isset($_GET['nome'])){
         $_SESSION['lista_tarefas'][] = $_GET['nome'];
     }
@@ -29,17 +13,8 @@ $lista_tarefas = array();
 
     if (isset ($_SESSION['lista_tarefas'])) {
         $lista_tarefas = $_SESSION['lista_tarefas'];
+    }else{
+        $lista_tarefas = array();
     }
-?>
-<table>
-    <tr>
-        <th>Tarefas</th>
-</tr>
-<?php foreach ($lista_tarefas as $tarefa) : ?>
-    <tr>
-        <td><?php echo $tarefa; ?> </td>
-</tr>
-<?php endforeach; ?>
-</table>
-</body>
-</html>
+
+include "template.php"
